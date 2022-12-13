8fb6c68349413a680a2f7356a5a427c8192b68cb
Sudipto Shahin <sudipto.shine@gmail.com>
2022-12-13 22:35:39 +0600

    the basic

--- basic02-installing/test1.js
+++ basic02-installing/test1.js
@@ -3,3 +3,70 @@ function greeter(person) {
 }
 var user = 'Sudipto';
 document.body.textContent = greeter(user);
+function logPoint(point) {
+    console.log("x = " + point.x + ", y = " + point.y);
+}
+function logName(x) {
+    console.log('Hello :', +x.name);
+}
+var obj = {
+    x: 0,
+    y: 0,
+    name: "Origin"
+};
+logPoint(obj);
+logName(obj);
+var Empty = /** @class */ (function () {
+    function Empty() {
+    }
+    return Empty;
+}());
+function fn(arg) {
+    console.log(' hello world');
+}
+fn({ k: 10 });
+// Function syntax includes parameter names
+var fst_ = function (a, b) { return a; };
+//  Object literal type syntax closely mirrors object literal value syntax:
+var o = { n: 1, xs: [] };
+// Boxed types
+(1).toExponential();
+var anys = [];
+anys.push(1);
+anys.push('hello');
+anys.push({ anything: 'goes' });
+// anys.map(anys[1]);
+// Structural typing
+// @strict: false
+var o1 = { x: 'hello', extra: 1 };
+var Three = /** @class */ (function () {
+    function Three() {
+        this.p = 'hello';
+    }
+    return Three;
+}());
+var x = { p: 'hello' };
+var two = x;
+two = new Three();
+function start(arg) {
+    if (typeof arg === 'string') {
+        return commonCase(arg);
+    }
+    else if (Array.isArray(arg)) {
+        return arg.map(commonCase).join(',');
+    }
+    else if (typeof arg === 'function') {
+        return commonCase(arg());
+    }
+    else {
+        return commonCase(arg.s);
+    }
+    function commonCase(s) {
+        // finally convert a string to another string
+        return s;
+    }
+}
+console.log(start('hello'));
+pad('hello', 10, 'left');
+var message = 'HEllo';
+console.log(message.toLowerCase());
--- basic02-installing/test1.ts
+++ basic02-installing/test1.ts
@@ -5,3 +5,109 @@ function greeter(person: string) {
 
 let user = 'Sudipto';
 document.body.textContent = greeter(user);
+
+/**============================= */
+
+interface Pointlike {
+    x: number;
+    y: number;
+  }
+  
+  interface Named {
+    name: string;
+  }
+  
+  function logPoint(point: Pointlike) {
+      console.log("x = "+point.x+ ", y = " +point.y);
+  }
+  
+  function logName(x: Named) {
+    console.log('Hello :', +x.name);
+  }
+  
+  const obj = {
+    x: 0,
+    y: 0,
+    name: "Origin"
+  };
+  
+  logPoint(obj);
+  logName(obj);
+  
+  class Empty {}
+  
+  function fn (arg: Empty) {
+    console.log(' hello world');
+  }
+  
+  fn({ k: 10 });
+  
+  
+  // Function syntax includes parameter names
+  let fst_: <T, U>(a: T, b: U) => T = (a, b) => a;
+  
+  //  Object literal type syntax closely mirrors object literal value syntax:
+  
+  let o: { n: number; xs: object[] } = { n: 1, xs: [] };
+  
+  // Boxed types
+  (1).toExponential();
+  
+  const anys = [];
+  // anys.push(1);
+  // anys.push('hello');
+  // anys.push({ anything: 'goes' });
+  
+  // anys.map(anys[1]);
+  
+  // Structural typing
+  
+  // @strict: false
+  let o1 = { x: 'hello', extra: 1 };
+  // let o2: { x: string } = 0;
+  
+  type One = { p: string };
+  
+  interface Two {
+    p: string;
+  }
+  
+  class Three {
+    p = 'hello';
+  }
+  
+  let x: One = { p: 'hello' };
+  let two: Two = x;
+  two = new Three();
+  
+  function start(
+    arg: string | string[] | (() => string) | { s: string }
+  ): string {
+    if (typeof arg === 'string') {
+      return commonCase(arg);
+    } else if(Array.isArray(arg)) {
+      return arg.map(commonCase).join(',');
+    } else if(typeof arg === 'function') {
+      return commonCase(arg());
+    } else {
+      return commonCase(arg.s);
+    }
+  
+  
+    function commonCase(s: string): string {
+      // finally convert a string to another string
+      return s;
+    }
+  }
+  
+  console.log(start('hello'));
+  
+  //  Unit types
+  declare function pad(s: string, n: number, direction: "left" | "right"): string;
+  
+  pad('hello', 10, 'left');
+  
+
+  const message = 'HEllo';
+
+  console.log(message.toLocaleLowerCase());
